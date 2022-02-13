dde111df6d1201372d7adf839aec2ac9fe75a39a
aleksandr-ponomarenko-branderstudio <99451010+aleksandr-ponomarenko-branderstudio@users.noreply.github.com>
2022-02-13 19:27:19 +0200

    Update lesson3_task3.php

--- lesson3_task3.php
+++ lesson3_task3.php
@@ -2,33 +2,17 @@
 $numberRandom = rand(1, 50);
 
 switch($numberRandom){
-    case 1:
-        echo $numberRandom, " - Odd";
-        break;
-    case 2:
-        echo $numberRandom, " - Odd";
-        break;
-    case 3:
-        echo $numberRandom, " - Odd";
-        break;
-    case 4:
-        echo $numberRandom, " - Odd";
-        break;
+    case 1:        
+    case 2:       
+    case 3:      
+    case 4:       
     case 5:
         echo $numberRandom, " - Odd";
         break;
-    case 10:
-        echo $numberRandom, " - Even";
-        break;
+    case 10:     
     case 20:
-        echo $numberRandom, " - Even";
-        break;
     case 30:
-        echo $numberRandom, " - Even";
-        break;
     case 40:
-        echo $numberRandom, " - Even";
-        break;
     case 50:  
         echo $numberRandom, " - Even";
         break;  
