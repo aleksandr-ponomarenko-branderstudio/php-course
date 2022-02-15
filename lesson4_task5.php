039ac9aac3e1029013c9e5a992da11ba34904e42
aleksandr-ponomarenko-branderstudio <99451010+aleksandr-ponomarenko-branderstudio@users.noreply.github.com>
2022-02-16 01:29:19 +0200

    Update lesson4_task5.php

--- lesson4_task5.php
+++ lesson4_task5.php
@@ -1,6 +1,18 @@
 <?php
 
+function divider($num){
+    for($i = 1; $i <= $num; $i++){
+        if($num % $i === 0){
+            $arreyDevider[] = $i;
+        }
+    }
+    return $arreyDevider;
+    
+}
 
+
+$arreyDevider[] = divider(21);
+print_r($arreyDevider);
 
 ?>
 
